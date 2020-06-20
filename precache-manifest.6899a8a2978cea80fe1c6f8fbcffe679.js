1932720ee9f20c2386c434dd3b701b2fa97558c7
Jagan1992 <Jagadesh043@gmail.com>
2020-06-20 18:21:26 +0530

    Updates

---
+++ precache-manifest.6899a8a2978cea80fe1c6f8fbcffe679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6367a36d9ba8959b83be636f97e574b6",
+    "url": "/monsters-rolodics/index.html"
+  },
+  {
+    "revision": "ae3aef3994662ad1dcee",
+    "url": "/monsters-rolodics/static/css/main.233b4705.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodics/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodics/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3aef3994662ad1dcee",
+    "url": "/monsters-rolodics/static/js/main.b75dfdf2.chunk.js"
+  },
+  {
+    "revision": "730c6e7d2ce6cdc8c9c9",
+    "url": "/monsters-rolodics/static/js/runtime-main.88defd78.js"
+  }
+]);
