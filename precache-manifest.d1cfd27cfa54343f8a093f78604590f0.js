395d5441fdd74055c00f6e9ae2b0dfd67acdb5aa
Jagan1992 <45116285+Jagan1992@users.noreply.github.com>
2020-06-20 15:13:23 +0530

    Added files of monsters-rolodex app to main branch
    Added files of monsters-rolodex app to main branch.

---
+++ precache-manifest.d1cfd27cfa54343f8a093f78604590f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ca68db1be3b08cb2b909acdd61d6f6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "07ff05228a997718bf2d",
+    "url": "/monsters-rolodex/static/css/main.876aaca8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ff05228a997718bf2d",
+    "url": "/monsters-rolodex/static/js/main.053ce0e8.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
